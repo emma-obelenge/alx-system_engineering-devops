72bf8c47636e58b4272f50cfecad78fcf48b3c5a
emma-obelenge <obelemmanuel@gmail.com>
2024-04-23 15:36:41 -0700

    puppet project done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-package { 'python3-pip':
-    ensure => installed,
-}
+# install puppet-lint -v 2.5.0
 
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    path    => '/usr/local/bin:/usr/bin:/bin',
-    require => Package['python3-pip'],
+exec { 'puppet-lint':
+      command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
