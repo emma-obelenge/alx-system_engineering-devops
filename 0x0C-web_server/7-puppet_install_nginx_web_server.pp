0b450a5f3eeef91447abc8e43078c179575b200f
Emmanuel Obelenge <obeleemmanuel@gmail.com>
2024-05-20 11:14:26 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,7 +4,7 @@ package { 'nginx':
   ensure     => 'installed',
 }
 
-file { '/home/ubuntu/index.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
@@ -12,7 +12,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://youtube.com/@emma_obelenge?si=x6uGvH6HZ9hoU-CH permanent;',
+  line   => 'location /redirect_me {return 301 https://youtube.com/@emma_obelenge?si=x6uGvH6HZ9hoU-CH permanent;}',
 }
 
 service { 'nginx':
