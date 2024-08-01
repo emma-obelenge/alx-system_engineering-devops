b3f2a5307993bea2d75f7fd0c174803f17a25f6e
emma-obelenge <obeleemmanuel@gmail.com>
2024-08-01 08:24:12 -0700

    api project task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Program that uses Employee ID to return information about their TODO list"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        exit()
+    api_url = 'https://jsonplaceholder.typicode.com'
+    usr_uri = '{}/users/{}'.format(api_url, employee_id)
+    todo_uri = '{}/todos'.format(usr_uri)
+
+    # User Response
+    response = requests.get(usr_uri).json()
+
+    # Name of employee
+    employee_name = response.get('name')
+
+    # User TODO Response
+    response = requests.get(todo_uri).json()
+
+    # Total num of tasks, both completed and non-completed
+    total = len(response)
+
+    # Number of incomplete tasks
+    incomplete = sum([elem['completed'] is False for elem in response])
+
+    # Number of completed tasks
+    complete = total - incomplete
+
+    # Formatting the expected output
+    str = "Employee {} is done with tasks({}/{}):"
+    print(str.format(employee_name, complete, total))
+
+    # Printing completed tasks
+    for elem in response:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))
+"""
+def employee_info(emp_id):
+    url = f'https://jsonplaceholder.typicode.com/todos/{emp_id}'
+
+    with urllib.request.urlopen(url) as response:
+        response_data = response.read().decode()
+        data = json.loads(response_data)
+        return (data)
+
+strings = employee_info(employee_id)
+print(f"type is {type(strings)} and data is: \n{strings}")
+"""
